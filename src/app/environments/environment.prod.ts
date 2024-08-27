0cc07fd22bca79c11b45493ddaa93d499d5de13a
Sergio Márquez <sergiomarquezdev@gmail.com>
2024-08-27 19:26:22 +0200

    environments

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   openAiApiKeyBlog:
     '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',
   pgHost: 'ep-shrill-limit-a2ct6evv-pooler.eu-central-1.aws.neon.tech',
