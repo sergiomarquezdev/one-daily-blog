d4a8ab7ad93573582aad50be465e814c81853345
Sergio Márquez <sergiomarquezdev@gmail.com>
2024-08-27 19:09:58 +0200

    default process.env environment

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  openAiApiKeyBlog: '',
-  pgHost: '',
-  pgDatabase: '',
-  pgUser: '',
-  pgPassword: '',
+  openAiApiKeyBlog: process.env['OPENAI_API_KEY_BLOG'],
+  pgHost: process.env['PGHOST'],
+  pgDatabase: process.env['PGDATABASE'],
+  pgUser: process.env['PGUSER'],
+  pgPassword: process.env['PGPASSWORD'],
 };
