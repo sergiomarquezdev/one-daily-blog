b4e3bef5c762f081a7fb9df981fb8379fcfa9231
Sergio Márquez <sergiomarquezdev@gmail.com>
2024-08-27 19:38:18 +0200

    environment.ts file

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  openAiApiKeyBlog:
+    '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',
+  pgHost: 'ep-shrill-limit-a2ct6evv-pooler.eu-central-1.aws.neon.tech',
+  pgDatabase: 'marquesitodb',
+  pgUser: 'blog_user',
+  pgPassword: 'taxzkee_blog_password',
+};
