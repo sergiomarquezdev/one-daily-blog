a6dfeaa2a08cb29fc9953752c294b287a9a5c51e
Sergio Márquez <sergiomarquezdev@gmail.com>
2024-08-28 17:24:45 +0200

    fix environments directory

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Post } from '../entities/Post';
-import {environment} from "../environments/environment";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root',
