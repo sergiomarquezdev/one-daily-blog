cc2c3f809fa277d86805087cd9940d15d8993543
Sergio Márquez <sergiomarquezdev@gmail.com>
2024-08-30 01:09:59 +0200

    Revert ssr

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -3,12 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Post } from '../entities/Post';
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
-  private apiUrl = '/api/posts';
+  private apiUrl = environment.apiUrl + '/posts';
 
   constructor(private http: HttpClient) {}
 
